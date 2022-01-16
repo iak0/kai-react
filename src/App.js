15a74f4c054a5377a4477faf9700b7e0c159e938
Kai Si <kai.si@berkeley.edu>
2022-01-15 23:05:51 -0800

    Day 6: Fix broken dropdowns, add better comments
    Separated "Create Timers" button and "Start/Stop/Resume" button

--- src/App.js
+++ src/App.js
@@ -16,6 +16,24 @@ function App() {
   );
 }
 
+/**
+ * This is a React Component which displays a multi-player timer.
+ * 
+ * The state for this component looks something like:
+ * this.state: {
+ *   players: [
+ *     {
+ *       seconds: 600,
+ *     },
+ *     {
+ *       seconds: 600,
+ *     }
+ *   ]
+ *   currentPlayer: 0,
+ *   numPlayers: 2,
+ *   initialTimer: 600s
+ * }
+ */
 class Timer extends React.Component {
   constructor() {
     super();
@@ -27,14 +45,18 @@ class Timer extends React.Component {
       currentPlayer: 0,
       players: this.makePlayers(defaultNumPlayers, initialSeconds),
     };
-    this.timer = 0;
-    this.startTimer = this.startTimer.bind(this);
+    this.timer = null; 
+    this.createTimers = this.createTimers.bind(this);
+    this.toggleTimer = this.toggleTimer.bind(this);
     this.countDown = this.countDown.bind(this);
     this.numPlayersChange = this.numPlayersChange.bind(this);
     this.initialTimeChange = this.initialTimeChange.bind(this);
     this.switchPlayer = this.switchPlayer.bind(this);
   }
 
+  /**
+   * Converts an integer number of seconds into a string in the MM:SS format.
+   */
   secondsToTime(secs){
     let minutes = Math.floor(secs / 60);
     let seconds = secs - (minutes * 60);
@@ -45,15 +67,15 @@ class Timer extends React.Component {
     return `${minutes}:${seconds}`;
   }
 
-  makeState(defaultNumPlayers, initialSeconds) {
-    return { 
-      numPlayers: defaultNumPlayers, 
-      initialSeconds: initialSeconds,
-      currentPlayer: 0,
-      players: this.makePlayers(defaultNumPlayers, initialSeconds),
-    }
-  }
-
+  /**
+   * Based on the number of players and initial time chosen, returns a list of players.
+   * 
+   * e.g. numPlayers = 2, initialSeconds = 600 returns a list:
+   *   [
+   *     { seconds: 600 }, 
+   *     { seconds: 600 }
+   *   ]
+   */ 
   makePlayers(numPlayers, initialSeconds) {
     let players = [];
     for (let i = 0; i < numPlayers; i++) {
@@ -64,83 +86,79 @@ class Timer extends React.Component {
     return players;
   }
 
-  componentDidMount() {
-    
+  /**
+   * This is called when the "Create Timers" button is pressed.
+   * Based on the number of players and initial time chosen, creates the
+   * correct number of players and timers.
+   * 
+   * This does not start the countdown on the timers.
+   */ 
+  createTimers() {
+    this.setState({
+      players: this.makePlayers(this.state.numPlayers, this.state.initialSeconds)
+    });
   }
 
-  startTimer() {
-    // this.setState(this.makeState(this.numPlayers, this.initialSeconds))
-    if (this.timer == 0) {
-      this.timer = setInterval(this.countDown, 1000);
+  /**
+   * This is called when the "Start/Pause/Resume" button is pressed.
+   * Starts counting down on the timers if it is not running (timer = null). 
+   * Otherwise, it stops the timer countdown and sets the timer to null.
+   */
+  toggleTimer() {
+    // TODO: prevent timer from being started if any of the players' times is <= 0
+    if (this.timer == null) {
+      // starts calling this.countdown() every second
+      this.timer = setInterval(this.countDown, 1000); 
     } else {
+      // stop calling this.countdown() every second
       clearInterval(this.timer);
-      this.timer = 0;
+      this.timer = null;
     }
   }
 
+  /**
+   * This is called once per second when the timer is active.
+   */
   countDown() {
-    // Remove one second, set state so a re-render happens.
-    let seconds = this.state.players[this.state.currentPlayer].seconds - 1;
+    // Remove one second
+    let players = this.state.players
+    let seconds = players[this.state.currentPlayer].seconds - 1;
 
-    this.state.players[this.state.currentPlayer].seconds = seconds;
+    players[this.state.currentPlayer].seconds = seconds;
 
+    // set state so a re-render happens.
     this.setState({
-      players: this.state.players,
+      players: players,
     });
     
     // Check if we're at zero.
-    if (seconds == 0) { 
+    if (seconds === 0) { 
       clearInterval(this.timer);
     }
   }
 
+  /**
+   * This is called when the user makes a selection in the "Number of Players" dropdown
+   */
   numPlayersChange(event) {
     this.setState({ numPlayers: parseInt(event.target.value) });
   }
 
+  /**
+   * This is called when the user makes a selection in the "Initial time" dropdown
+   */
   initialTimeChange(event) {
     this.setState({ initialSeconds: parseInt(event.target.value) * 60 });
   }
 
+  /**
+   * This is called when the "Next Player" button is called.
+   */
   switchPlayer() {
     let nextPlayer = (this.state.currentPlayer + 1) % this.state.numPlayers;
     this.setState({ currentPlayer: nextPlayer });
   }
 
-  /*
-  this.state: {
-    players: [
-      {
-        seconds: 600,
-      },
-      {
-        seconds: 600,
-      },
-      {
-        seconds: 600,
-      },
-      {
-        seconds: 600,
-      }
-    ]
-    currentPlayer: 3,
-    numPlayers: 4
-  }
-
-  players = []
-  for ( 4 times ) {
-    players.append {
-      seconds: beginningSeconds
-    }
-  }
-
-  this.state.players[currentPlayer].seconds
-
-  this.timer: every second, subtract 1 from the current player's seconds
-
-  we'll also need buttons for incrementing the currentPlayer to the next player
-
-  */
   render() {
     console.log(this.state);
     return (
@@ -155,11 +173,14 @@ class Timer extends React.Component {
         <br/>
         <b>Initial timer: </b>
         <select defaultValue="10" onChange={this.initialTimeChange}>
+          <option value="1">1</option>
           <option value="5">5</option>
           <option value="10">10</option>
           <option value="15">15</option>
           <option value="20">20</option>
         </select>
+        <br/>
+        <button onClick={this.createTimers}>Create Timers</button>
         <br/><br/>
         <div>
           {this.state.players.map((player, i) => {
@@ -168,7 +189,8 @@ class Timer extends React.Component {
               </div>
           })}
         </div>
-        <button onClick={this.startTimer}>Start</button>
+        <button onClick={this.toggleTimer}>Start/Pause/Resume</button>
+        <br/>
         <button onClick={this.switchPlayer}>Next Player</button>
       </div>
     );
